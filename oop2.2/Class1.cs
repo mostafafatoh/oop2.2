cc5140fd0e8f2ea3145f711cebffd252c233d99f
mostafa fattouh <moustafafattouh55@gmail.com>
2025-09-03 20:44:05 +0300

    Add project files.

---
+++ oop2.2/Class1.cs
@@ -0,0 +1,14 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2._2
+{
+    internal class Car
+    {
+        public int number;
+        public double price;
+    }
+}
---
+++ oop2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace oop2._2
+{
+    internal class Program
+    {static void Test(Car car)
+        {
+            int x = 3;
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Car c = new Car();
+            
+            c.price = 1999;
+            Car c2 = c; 
+            Console.WriteLine(c.price);
+            //c= new Car();
+            //c.price=5555;
+            Test(c);
+        }
+    }
+}
